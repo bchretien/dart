195cd7ed5f462b9c5c4a0bca48052ba5a63d8f7d
Jeongseok Lee <jslee02@gmail.com>
2014-06-10 20:11:59 -0400

    Fixing style

--- dart/renderer/OpenGLCamera.h
+++ dart/renderer/OpenGLCamera.h
@@ -40,26 +40,45 @@
 namespace dart {
 namespace renderer {
 
-class OpenGLCamera {
-
+/// class OpenGLCamera
+class OpenGLCamera
+{
 public:
-    OpenGLCamera() : public Camera {}
-      virtual ~OpenGLCamera() {};
-    virtual void set(const Vector3d& _Eye, const Vector3d& _look, const Vector3d& _up);
-    virtual void slide(double _delX, double _delY, double _delZ, bool _bLocal = false);
-    virtual void setFrustum(float _vAng, float _asp, float _nearD, float _farD);
-    virtual void setOrtho(float _width, float _height, float _nearD, float _farD);
+  ///
+  OpenGLCamera() : public Camera {}
 
+  ///
+  virtual ~OpenGLCamera() {}
 
-    virtual void roll(float _angle);
-    virtual void pitch(float _angle);
-    virtual void yaw(float _angle);
-    virtual void localRotate(float _angle, AXIS _axis);
-    virtual void globalRotate(float _angle, AXIS _axis);
+  ///
+  virtual void set(const Vector3d& _Eye, const Vector3d& _look, const Vector3d& _up);
 
+  ///
+  virtual void slide(double _delX, double _delY, double _delZ, bool _bLocal = false);
+
+  ///
+  virtual void setFrustum(float _vAng, float _asp, float _nearD, float _farD);
+
+  ///
+  virtual void setOrtho(float _width, float _height, float _nearD, float _farD);
+
+  ///
+  virtual void roll(float _angle);
+
+  ///
+  virtual void pitch(float _angle);
+
+  ///
+  virtual void yaw(float _angle);
+
+  ///
+  virtual void localRotate(float _angle, AXIS _axis);
+
+  ///
+  virtual void globalRotate(float _angle, AXIS _axis);
 };
 
 } // namespace renderer
 } // namespace dart
 
-#endif // #ifndef DART_RENDERER_OPENGLCAMERA_H
+#endif  // DART_RENDERER_OPENGLCAMERA_H_
